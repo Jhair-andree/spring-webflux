2f9ca079a92bddd77b13418dbe68b4b02a37f5cb
Martín <magadiflo@gmail.com>
2022-10-04 12:25:41 -0500

    Test endpoint listar

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Sección 5: API RESTFULL usando RestController
+
+# Ejecutar prueba unitaria
+En la ruta raíz del proyecto, mediante cmd ejecutar
+```
+mvnw.cmd test
+```
--- src/test/java/com/magadiflo/webflux/api/rest/app/SpringBootWebfluxApiRestApplicationTests.java
+++ src/test/java/com/magadiflo/webflux/api/rest/app/SpringBootWebfluxApiRestApplicationTests.java
@@ -1,13 +1,28 @@
 package com.magadiflo.webflux.api.rest.app;
 
+import com.magadiflo.webflux.api.rest.app.models.documents.Producto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
-class SpringBootWebfluxApiRestApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SpringBootWebfluxApiRestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    public void listarTest() {
+        this.client.get()
+                .uri("/api/v2/productos")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Producto.class)
+                .hasSize(12 );
+    }
 
 }
