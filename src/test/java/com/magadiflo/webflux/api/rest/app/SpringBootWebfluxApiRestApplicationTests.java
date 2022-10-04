4e2c5a6c5c8f69245bca071cb49454e0a732d10c
Martín <magadiflo@gmail.com>
2022-10-04 12:39:58 -0500

    Test endpoint listar usando consumeWith

--- src/test/java/com/magadiflo/webflux/api/rest/app/SpringBootWebfluxApiRestApplicationTests.java
+++ src/test/java/com/magadiflo/webflux/api/rest/app/SpringBootWebfluxApiRestApplicationTests.java
@@ -1,11 +1,14 @@
 package com.magadiflo.webflux.api.rest.app;
 
 import com.magadiflo.webflux.api.rest.app.models.documents.Producto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringBootWebfluxApiRestApplicationTests {
@@ -22,7 +25,15 @@ public class SpringBootWebfluxApiRestApplicationTests {
                 .expectStatus().isOk()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
                 .expectBodyList(Producto.class)
-                .hasSize(12 );
+                .consumeWith(response -> {
+                    List<Producto> productos = response.getResponseBody();
+                    System.out.println("Total de productos: " + productos.size());
+                    productos.forEach(p -> {
+                        System.out.println(p.getNombre());
+                    });
+                    Assertions.assertThat(productos.size() > 0).isTrue();
+                });
+                //.hasSize(12);
     }
 
 }
