3b37022814f4234f5eecd76ab1dc0047936d11d1
Martín <magadiflo@gmail.com>
2022-10-07 12:37:06 -0500

    Conctando nuestros servicios como clientes Eureka

--- src/main/java/com/magadiflo/webflux/api/rest/app/SpringBootWebfluxApiRestApplication.java
+++ src/main/java/com/magadiflo/webflux/api/rest/app/SpringBootWebfluxApiRestApplication.java
@@ -8,11 +8,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import reactor.core.publisher.Flux;
 
 import java.util.Date;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringBootWebfluxApiRestApplication implements CommandLineRunner {
 
